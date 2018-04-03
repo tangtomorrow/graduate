9342b7059176278c5ed6864fbc88003104238de9
tangtomorrow <tangyaming_nju@sina.com>
2018-04-03 12:40:18 +0800

    refactor data path

--- src/main/java/blinking/step2/ReadData.java
+++ src/main/java/blinking/step2/ReadData.java
@@ -1,14 +1,8 @@
 package blinking.step2;
 
-import com.google.common.base.Splitter;
-
 import java.io.IOException;
 
 public class ReadData {
-    private static final Splitter splitter = Splitter.on(",").trimResults();
-    private static final int expectedColNamesSize = 25;
-    private static final int expectedColValuesSize = 26;
-
     public static void main(String[] args) throws IOException {
     }
 
